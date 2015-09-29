307d5132f7f0b433e119786bceb635097533d892
jxie007 <jxie007@e.ntu.edu.sg>
2015-09-30 00:00:27 +0800

    first commit

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php 
+$pageTitle = "Contact Mike"; 
+
+$section = "contacts";
+
+
+include('include/header.php'); 
+?>
+
+	<div class="section page">
+		<h1>Contact</h1>
+	</div>
+
+<?Php 
+include('include/footer.php'); 
+?>
---
+++ shirts.php
@@ -0,0 +1,9 @@
+<?php 
+$pageTitle = "Mike's Full Catatlog of Shirts";
+$section = "shirts";
+include('include/header.php'); 
+?>
+	<div class="section page">
+		<h1>Mike&rsquo; Full Catalog of Shirts</h1>
+	</div>
+<?php include('include/footer.php'); ?>
