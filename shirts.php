13b995f3f050a058302808edecc854bc5ad139a5
jxie007 <jxie007@e.ntu.edu.sg>
2015-10-02 13:13:35 +0800

    create products array

--- shirts.php
+++ shirts.php
@@ -1,9 +1,26 @@
 <?php 
+	$products = array();
+	$products[101] = "Logo Shirt, Red";
+	$products[102] = "Mike the Frog Shirt, Black";
+	$products[103] = "Mike the Frog Shirt, Blue";
+	$products[104] = "Logo Shirt, Green";
+?>
+<?php 
 $pageTitle = "Mike's Full Catatlog of Shirts";
 $section = "shirts";
 include('include/header.php'); 
 ?>
 	<div class="section page">
-		<h1>Mike&rsquo; Full Catalog of Shirts</h1>
+		<div class="wrapper">
+			<h1>Mike&rsquo; Full Catalog of Shirts</h1>
+			<ul>
+				<?php foreach($products as $product){ ?>
+				<li>
+					<?php echo $product; ?>
+				</li>
+				<?php } ?>
+			</ul>	
+		</div>
+		
 	</div>
 <?php include('include/footer.php'); ?>
