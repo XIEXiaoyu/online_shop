634be8369b85296809077f08d14b038d7ad37e9f
jxie007 <jxie007@e.ntu.edu.sg>
2015-10-07 21:33:41 +0800

    integrating paypal payment

--- include/products.php
+++ include/products.php
@@ -3,41 +3,49 @@
 	$products[101] = array(
 	    "name" => "Logo Shirt, Red",
 	    "img" => "img/shirts/shirt-101.jpg",
-	    "price" => 18   
+	    "price" => 18,
+	    "paypal" => "XDZ3ZMLUVVRSA" 
 	);
 	$products[102] = array(
 	    "name" => "Mike the Frog Shirt, Black",
 	    "img" => "img/shirts/shirt-102.jpg",
-	    "price" => 20
+	    "price" => 20,
+	    "paypal" => "J5ZAZ7WAMAYZW"
 	);
 	$products[103] = array(
 	    "name" => "Mike the Frog Shirt, Blue",
 	    "img" => "img/shirts/shirt-103.jpg",    
-	    "price" => 20
+	    "price" => 20,
+	    "paypal" => "Q93H7CKXTHFQS"
 	);
 	$products[104] = array(
 	    "name" => "Logo Shirt, Green",
 	    "img" => "img/shirts/shirt-104.jpg",    
-	    "price" => 18
+	    "price" => 18,
+	    "paypal" => "69PZ9B5QWMZRC"
 	);
 	$products[105] = array(
 	    "name" => "Mike the Frog Shirt, Yellow",
 	    "img" => "img/shirts/shirt-105.jpg",    
-	    "price" => 25
+	    "price" => 25,
+	    "paypal" => "BVGC8PRHQRS2G"
 	);
 	$products[106] = array(
 	    "name" => "Logo Shirt, Gray",
 	    "img" => "img/shirts/shirt-106.jpg",    
-	    "price" => 20
+	    "price" => 20,
+	    "paypal" => "KQ7YK8PWF5MGN"
 	);
 	$products[107] = array(
 	    "name" => "Logo Shirt, Turquoise",
 	    "img" => "img/shirts/shirt-107.jpg",    
-	    "price" => 20
+	    "price" => 20,
+	    "paypal" => "8EALXGQ9M4W54"
 	);
 	$products[108] = array(
 	    "name" => "Logo Shirt, Orange",
 	    "img" => "img/shirts/shirt-108.jpg",    
 	    "price" => 25,
+	    "paypal" => "U27PCBMN2PAWW"
 	);
 ?>
--- shirt.php
+++ shirt.php
@@ -38,6 +38,30 @@ include('include/header.php'); ?>
 
 			<div class="shirt-details">
 				<h1><span class="price">$<?php echo $product['price']; ?></span> <?php echo $product['name']; ?></h1>
+
+				<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="<?php echo $product["paypal"]; ?>"> 
+				<input type="hidden" name="item_name" value="<?php echo $product["name"]; ?>">
+				<table>
+					<tr>
+						<th>
+							<input type="hidden" name="on0" value="Size">
+							<label for="os0">Size</label>
+						</th>
+						<td>
+							<select name="os0" id="os0">
+							<option value="Small">Small </option>
+							<option value="Medium">Medium </option>
+							<option value="Large">Large </option>
+							<option value="X-Large">X-Large</option>
+							</select> 
+						</td>
+					</tr>
+				</table>
+				<input type="submit" value="Add to Cart" name="submit">
+				</form>
+
 				<p class="note-designer">* All shirts are designed by Mike the Frog.</p>				
 				
 			</div>
